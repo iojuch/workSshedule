8c86f52e12fa52531f589f8e017ecf77b7884f7d
iojuch <petrovich8384@mail.ru>
2023-09-10 16:57:44 +0300

    File csv added

--- src/test/java/ru/netology/workSchedule/services/WorkScheduleServiceTest.java
+++ src/test/java/ru/netology/workSchedule/services/WorkScheduleServiceTest.java
@@ -3,15 +3,16 @@ package ru.netology.workSchedule.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class WorkScheduleServiceTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "3,10000,3000,20000",
-            "2,100000,60000,150000"
-    })
+    //@CsvSource({
+    //        "3,10000,3000,20000",
+    //        "2,100000,60000,150000"
+    @CsvFileSource(files = "src/test/resources/workSchedule.csv")
 
     public void shouldWorkSchedule(int expected, int income, int expenses, int threshold) {
         WorkScheduleService service = new WorkScheduleService();
